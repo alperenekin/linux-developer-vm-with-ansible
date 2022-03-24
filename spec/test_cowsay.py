05299b2bfd5b8d92779a2d1b7e6b70786fedc29b
alperenekin <alperen.ekin@zuehlke.com>
2022-03-24 17:43:47 +0100

    Typo fixed, test passes

--- spec/test_cowsay.py
+++ spec/test_cowsay.py
@@ -4,5 +4,5 @@ def test_cowsay_is_installed(host):
     assert host.package('cowsay').is_installed
 
 def test_cowsay_command_is_found(host):
-    cmd = host.run("/user/games/cowsay hello")
+    cmd = host.run("/usr/games/cowsay hello")
     assert cmd.rc is 0
